16c16a48072e99d3997718920ec2caf0e1b0a517
intanon55 <53069180+intanon55@users.noreply.github.com>
2020-12-22 09:18:32 +0700

    Update done.vue

--- pages/register/done.vue
+++ pages/register/done.vue
@@ -42,7 +42,7 @@ export default {
         register() {
       if(this.validate()){
         this.$store.dispatch('setRegister', this.form)
-        this.$axios.patch(`https://hffchfd-fe5c4.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`, this.$store.getters.getRegister).then((res) => {
+        this.$axios.patch(`https://recipt-7f485.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`, this.$store.getters.getRegister).then((res) => {
           this.$router.push('/register/done')
         }).catch(e => console.log(e))         
       }
