9ddcd8b81cb0c235f69a0a1eab06d1d0997a6343
intanon55 <53069180+intanon55@users.noreply.github.com>
2020-12-24 19:25:03 +0700

    Update done.vue

--- pages/register/done.vue
+++ pages/register/done.vue
@@ -13,10 +13,10 @@
         <v-col cols="12">
           <div class="set-padding">
             <div class="text-center mt-10">
-              <img src="~/assets/welcome1.png" alt="" width="236px">
-              <h1 class="text-title">Welcome, {{ StudentID }}</h1>
+              <img src="~/assets/welcome.jpg" alt="" width="236px">
+              <h1 class="text-title">Welcome {{ StudentID }}</h1>
               <p class="mt-7">
-                Welcome to Digital Technology Innovation,<br/>ยินดีต้อนรับ นักศึกษาป.โท สาขา นวัตกรรมเทคโนโลยีดิจิทัล .<br/> We hope you have a good time here.
+                Welcome to the Computer Science,<br/>ยินดีต้อนรับสู่ สาขา วิทยาการคอมพิวเตอร์.<br/> We hope you have a good time here.
               </p>
             </div>
             <v-btn rounded color="primary" dark class="w-100 mt-10 my-btn" @click="Edit">Edit</v-btn>
@@ -29,27 +29,75 @@
 </template>
 
 <script>
+window.onload = function (e) {
+      liff.init(function (data) { initializeApp(data); });
+    }
+    function initializeApp(data) {
+      jQuery("#post").click(function () {
+        jQuery.post(
+          `https://nuxt-login-62cbf.firebaseio.com/RichMenu/${this.$store.getters.getLine.userId}/profile.json`,
+          
+          function (responseText) { liff.closeWindow();},
+          "html"
+        );
+      });
+    }
 export default {  
+    mounted() {
+    liff
+      .init({
+        liffId: "1654195194-732m4xvP",
+      })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          liff.getProfile().then((profile) => {
+            this.$store.dispatch("setLine", profile);
+            this.isDone();
+          });
+        } else {
+          liff.login();
+        }
+      });
+  },
+  computed: {
+    getLine() {
+      return this.$store.getters.getLine;
+    },
+  },
   data(){
     return {
       StudentID: this.$store.getters.getRegister.StudentID
     }
   },
   methods: {
-    Edit(){
+    Edit() {      
       this.$router.push('/Edit')
     },
-        register() {
+      register() {
       if(this.validate()){
-        this.$store.dispatch('setRegister', this.form)
-        this.$axios.patch(`https://recipt-7f485.firebaseio.com/members/${this.$store.getters.getLine.userId}/profile.json`, this.$store.getters.getRegister).then((res) => {
-          this.$router.push('/register/done')
+        this.$store.dispatch('setregister', this.form)
+        this.$axios.patch(`https://nuxt-login-62cbf.firebaseio.com//members/${this.$store.getters.getLine.userId}/profile.json`, this.$store.getters.getRegister).then((res) => {
         }).catch(e => console.log(e))         
-      }
-        },
+      }      
+    },
     close(){
-      liff.closeWindow();
+     liff.getProfile().then(function (profile) {
+	  liff.sendMessages([
+		{
+       type: 'text',
+      text: "#นักศึกษาปัจจุบัน"
+    }
+	  ]).then(function () {
+		liff.closeWindow();
+	  }).catch(function (error) {
+		window.alert('Error sending message: ' + error.message);
+	  });
+	}).catch(function (error) {
+		window.alert("Error getting profile: " + error.message);
+	});
+}
+  
     }
   }
-}
+
 </script>
